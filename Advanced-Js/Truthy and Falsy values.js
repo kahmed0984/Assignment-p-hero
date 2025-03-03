0ef7ab953e232a99f36a2f18285188efbae10a74
kahmed0984 <kahmed0984@gmail.com>
2025-03-03 14:56:35 +0600

    add some

---
+++ Advanced-Js/Truthy and Falsy values.js
@@ -0,0 +1,23 @@
+//Falsy:   0, "", undefined, null, NaN, false
+//Truthy:  3, " ", "0", [] empty array, {} empty object
+
+
+
+const age = 0 ;
+if(age){
+    console.log('contition is true');
+}
+else{
+    console.log('contition is false');
+}
+
+
+
+let name = NaN ;
+console.log(name);
+if(name){
+    console.log('contition is true');
+}
+else{
+    console.log('contition is false');
+}
